f230106c5e397fa0bbb2aace27181bc4a5c987ed
acarrm04@estudiantes.unileon.es <114087227+AlonsoCarreraMartinez@users.noreply.github.com>
2026-06-16 05:58:41 +0200

    fix problem

--- client/src/lib/dashboard-context.tsx
+++ client/src/lib/dashboard-context.tsx
@@ -65,7 +65,7 @@ export function DashboardProvider({ children }: { children: ReactNode }) {
 
       try {
         //const API_BASE = (import.meta as any).env.VITE_API_URL
-        const API_BASE = https://alonsocarreramartinez-space.hf.space
+        const API_BASE = "https://alonsocarreramartinez-space.hf.space"
         if (!API_BASE) throw new Error("VITE_API_URL is missing!")
         
         const response = await fetch(`${API_BASE}/api/auth/verify`, {
